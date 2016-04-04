2c490e67ea885ec238426ad222d4a6208085ded0
ashirwad08 <ashirwad08@yahoo.com>
2016-04-04 13:03:01 -0700

    correcting matrix_algebra.py based on Ling's feedback

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -5,25 +5,25 @@ import numpy as np
 
 def main():
   #Matrices dict
-  Mat = {'A': np.array([[1,2,3],[2,7,4]]), 'B': np.array([[1,-1],[0,1]]), 'C': np.array([[5,-1],[9,1],[6,0]]), 'D': np.array([[3, -2, -1],[1,2,3]]), 'u': np.array([6,2,-3,5]), 'v': np.array([3,5,-1,4]), 'w': np.array([(1),(8),(0),(5)])}
+  Mat = {'A': np.array([[1,2,3],[2,7,4]]), 'B': np.array([[1,-1],[0,1]]), 'C': np.array([[5,-1],[9,1],[6,0]]), 'D': np.array([[3, -2, -1],[1,2,3]]), 'u': np.array([(6,2,-3,5)]), 'v': np.array([(3,5,-1,4)]), 'w': np.array([[1],[8],[0],[5]])}
   
   
   # SOLUTIONS
   
   ##1 Dimensions
   print('\n Matrix Dimensions:')
-  for k,v in Mat.iteritems():
-    print(k + "=" + str(v.ndim))
+  for k,v in sorted(Mat.items()):
+    print(k + "=" + str(v.shape))
     
   """
    Matrix Dimensions:
-  A=2
-  C=2
-  B=2
-  D=2
-  u=1
-  w=1
-  v=1
+  A=(2, 3)
+  B=(2, 2)
+  C=(3, 2)
+  D=(2, 3)
+  u=(1, 4)
+  v=(1, 4)
+  w=(4, 1)
   """
   
   #2 Vector Operations
@@ -31,8 +31,8 @@ def main():
   print("u+v = "+str(Mat['u']+Mat['v'])) #u+v = [ 9  7 -4  9]
   print("u-v = "+str(Mat['u']-Mat['v'])) #u-v = [ 3 -3 -2  1]
   print("6*u = "+str(6*Mat['u'])) #6*u = [ 36  12 -18  30]
-  print("u.v = "+str(Mat['u'].dot(Mat['v']))) #u.v = 51
-  print("||u|| = "+str(np.sqrt(sum(Mat['u']**2)))) #||u|| = 8.60232526704
+  print("u.v = "+str(Mat['u'].dot(Mat['v'].T)[0][0])) #u.v = 51
+  print("||u|| = "+str(np.sqrt(np.sum(Mat['u']**2)))) #||u|| = 8.60232526704
   
   #3 Matrix Operations
   print('\n Matrix Operations: ')
@@ -68,10 +68,10 @@ def main():
        [ 6, -6]]))
        """
        
-    print("B^4 = ", (Mat['B']**4))
+    print("B^4 = ", (np.dot(np.dot(np.dot(Mat['B'],Mat['B']), Mat['B']), Mat['B']))) #discovered i can also use numpy's linalg.matrix_power for this
     """
-    ('B^4 = ', array([[1, 1],
-       [0, 1]]))
+    ('B^4 = ', array([[ 1, -4],
+       [ 0,  1]]))
        """
        
     print("AA' = ", (Mat['A'].dot(Mat['A'].T)))
